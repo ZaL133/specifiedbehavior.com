621a69c67e07cb082a235087d293a780af340eaa
Levin <zeph.levin@gmail.com>
2023-06-03 11:12:03 -0400

    Restored TwoColumnLayout

---
+++ src/web/docs/src/.vuepress/components/TwoColumnLayout.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="two-column-layout">
+    <div class="column">
+      <slot name="left"></slot>
+    </div>
+    <div class="column">
+      <slot name="right"></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .two-column-layout {
+    display: flex;
+    flex-direction: row;
+  }
+  .column {
+    padding: 4px;
+  }
+</style>
